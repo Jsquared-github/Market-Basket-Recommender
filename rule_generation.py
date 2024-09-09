5374e6a1360af111230f76b2ea72fec8735b5d1b
Joshua Johnston <2021johnston.joshua@gmail.com>
2024-09-09 16:37:57 -0400

    generated rules using aprior, eclat, and fpgrowth

--- rule_generation.py
+++ rule_generation.py
@@ -11,17 +11,61 @@ with open("processed_data/train/tuple_itemsets.pkl", "rb") as tf:
 with open("processed_data/train/one_hot.pkl", "rb") as ohf:
     oh_transactions: pd.DataFrame = pkl.load(ohf)
 
-itemsets, rules = apriori(transactions, min_support=.01, min_confidence=.501)
-for rule in rules:
+final_rules = {}
+_, apriori_rules = apriori(transactions, min_support=.01)
+for rule in apriori_rules:
     sigmoid_conv = (2/(1 + exp(-(log(rule.conviction))))) - 1
     sigmoid_lift = (2/(1 + exp(-(abs(rule.lift-1))))) - 1
     sup_conf_score = rule.support*rule.confidence
     lift_conv_score = sigmoid_lift*sigmoid_conv
-    print(rule.lhs, rule.rhs, (sup_conf_score+lift_conv_score))
+    lhs = rule.lhs
+    rhs = rule.rhs
+    candidate_rule_score = (sup_conf_score+lift_conv_score)
 
-eclat_itemsets = eclat(oh_transactions, min_support=.05)
-print(eclat_itemsets)
+    if final_rules.get(lhs, False):
+        final_rhs: dict = final_rules.get(lhs)
+        for score in final_rhs.values():
+            if score < candidate_rule_score:
+                final_rules[lhs] = {rhs: candidate_rule_score}
+    else:
+        final_rules[lhs] = {rhs: candidate_rule_score}
 
-oh_transactions = oh_transactions.astype('bool')
-fp_itemsets = fpgrowth(oh_transactions, min_support=.01, use_colnames=True)
-print(fp_itemsets)
+_, eclat_rules = eclat(transactions, min_support=.005)
+for rule in eclat_rules:
+    sigmoid_conv = (2/(1 + exp(-(log(rule.conviction))))) - 1
+    sigmoid_lift = (2/(1 + exp(-(abs(rule.lift-1))))) - 1
+    sup_conf_score = rule.support*rule.confidence
+    lift_conv_score = sigmoid_lift*sigmoid_conv
+    lhs = rule.lhs
+    rhs = rule.rhs
+    candidate_rule_score = (sup_conf_score+lift_conv_score)
+
+    if final_rules.get(lhs, False):
+        final_rhs: dict = final_rules.get(lhs)
+        for score in final_rhs.values():
+            if score < candidate_rule_score:
+                final_rules[lhs] = {rhs: candidate_rule_score}
+    else:
+        final_rules[lhs] = {rhs: candidate_rule_score}
+
+
+_, fpgrowth_rules = fpgrowth(transactions, min_support=.001)
+for rule in fpgrowth_rules:
+    sigmoid_conv = (2/(1 + exp(-(log(rule.conviction))))) - 1
+    sigmoid_lift = (2/(1 + exp(-(abs(rule.lift-1))))) - 1
+    sup_conf_score = rule.support*rule.confidence
+    lift_conv_score = sigmoid_lift*sigmoid_conv
+    lhs = rule.lhs
+    rhs = rule.rhs
+    candidate_rule_score = (sup_conf_score+lift_conv_score)
+
+    if final_rules.get(lhs, False):
+        final_rhs: dict = final_rules.get(lhs)
+        for score in final_rhs.values():
+            if score < candidate_rule_score:
+                final_rules[lhs] = {rhs: candidate_rule_score}
+    else:
+        final_rules[lhs] = {rhs: candidate_rule_score}
+
+with open('models/association_rules.pkl', "wb") as f:
+    pkl.dump(final_rules, f)
