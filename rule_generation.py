3a99f40a8bc3910c6b5183133da70d1d9cbf3ae9
Joshua Johnston <2021johnston.joshua@gmail.com>
2024-09-09 13:00:48 -0400

    Convinction and lift scaling screenshots and refactor of conviction scaling equation

--- rule_generation.py
+++ rule_generation.py
@@ -1,6 +1,6 @@
 import pickle as pkl
 import pandas as pd
-from numpy import exp, abs
+from numpy import exp, abs, log
 from efficient_apriori import apriori
 from FIM import eclat
 from mlxtend.frequent_patterns import fpgrowth
@@ -13,7 +13,7 @@ with open("processed_data/train/one_hot.pkl", "rb") as ohf:
 
 itemsets, rules = apriori(transactions, min_support=.01, min_confidence=.501)
 for rule in rules:
-    sigmoid_conv = (2/(1 + exp(-(rule.conviction-1)))) - 1
+    sigmoid_conv = (2/(1 + exp(-(log(rule.conviction))))) - 1
     sigmoid_lift = (2/(1 + exp(-(abs(rule.lift-1))))) - 1
     sup_conf_score = rule.support*rule.confidence
     lift_conv_score = sigmoid_lift*sigmoid_conv
